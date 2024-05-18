1f949dfa824d49afd6b094634bc0b26e30a9967a
Avinashkumar s m <avinashkumar1998dgd@gmail.com>
2024-05-18 12:45:55 +0530

    4454

---
+++ Testproject/src/main/java/com/crm/cast/products/Productslink.java
@@ -0,0 +1,15 @@
+package com.crm.cast.products;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Productslink {
+@FindBy(name = "products")
+private WebElement productlink;
+
+public WebElement getProductlink() {
+	return productlink;
+}
+@FindBy(name = "products")
+private WebElement productl2ink;
+}
